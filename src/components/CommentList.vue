32eee5f51a91d840b012e9c1b120eaa1aa8732f6
inwoo jang <inuj03@naver.com>
2024-09-10 11:37:51 +0900

    Initial commit

---
+++ src/components/CommentList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ contentId }}
+    </div>
+</template>
+
+<script>
+import data from '@/data';
+
+export default {
+    name: 'CommentList',
+    props: {
+        contentId: Number,
+    },
+    data() {
+        return {
+            comments: data.Comment.filter(item => item.content_id === this.contentId)
+        }
+    }
+}
+
+</script>
